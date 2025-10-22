3ccf3e24589fc6389b77d1cc67a862a006ee1468
Jaded Burnout <jaded.burnout69@gmail.com>
2025-10-22 09:35:58 +0100

    Various fixups

--- spec/lib/models/record_spec.rb
+++ spec/lib/models/record_spec.rb
@@ -1,25 +1,27 @@
 require "spec_helper"
 require "models/record"
 
-class TestRecord < Record
-  attributes %I[
-    test
-    testing
-  ]
-end
+RSpec.describe Record do
+  let(:test_record_class) do
+    Class.new(Record) do
+      attributes %I[
+        test
+        testing
+      ]
+    end
+  end
 
-RSpec.describe TestRecord do
   it "assigns permitted attributes" do
-    record = described_class.new(test: "123")
+    record = test_record_class.new(test: "123")
     expect(record.test).to eq("123")
     expect(record.testing).to eq(nil)
     expect(record.respond_to?(:nonattribute)).to eq(false)
 
-    record = described_class.new(test: "123", testing: "567")
+    record = test_record_class.new(test: "123", testing: "567")
     expect(record.test).to eq("123")
     expect(record.testing).to eq("567")
 
-    record = described_class.new(test: "123", nonattribute: "567")
+    record = test_record_class.new(test: "123", nonattribute: "567")
     expect(record.test).to eq("123")
     expect(record.respond_to?(:nonattribute)).to eq(false)
   end
